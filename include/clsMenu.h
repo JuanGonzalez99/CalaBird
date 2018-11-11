a65f8566c6eb04fc9d2800cc3840cae7db3e8a80
Javier Pereyra <javier.pereyra183@gmail.com>
2018-11-11 18:38:51 -0300

    menu

---
+++ include/clsMenu.h
@@ -0,0 +1,17 @@
+#ifndef CLSMENU_H
+#define CLSMENU_H
+
+#include <clsSprite.h>
+
+
+class clsMenu : public clsSprite
+{
+    public:
+        clsMenu();
+
+    protected:
+
+    private:
+};
+
+#endif // CLSMENU_H
---
+++ src/clsMenu.cpp
@@ -0,0 +1,6 @@
+#include "clsMenu.h"
+
+clsMenu::clsMenu()
+{
+    //ctor
+}
