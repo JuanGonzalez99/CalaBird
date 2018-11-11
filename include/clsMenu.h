3ed16821ff319acc95567bb6de19733a197f7477
Javier Pereyra <javier.pereyra183@gmail.com>
2018-11-11 19:31:30 -0300

    mas imagenes

--- include/clsMenu.h
+++ include/clsMenu.h
@@ -2,16 +2,26 @@
 #define CLSMENU_H
 
 #include <clsSprite.h>
+#include <clsScreen.h>
+#include <clsMusic.h>
+#include <clsEvent.h>
+#include <clsText.h>
+#include <clsTimer.h>
 
 
 class clsMenu : public clsSprite
 {
     public:
-        clsMenu();
-
-    protected:
-
+       void inherit(){};
+       int init(clsScreen*, clsMusic*, clsEvent*, clsTimer*);
+       void opciones();
+       int run();
     private:
+        clsScreen *screen;
+        clsMusic *music;
+        clsEvent *event;
+        clsText texto;
+        clsTimer *timer;
 };
 
 #endif // CLSMENU_H
--- src/clsMenu.cpp
+++ src/clsMenu.cpp
@@ -1,6 +1,93 @@
 #include "clsMenu.h"
 
-clsMenu::clsMenu()
+int clsMenu::init(clsScreen *s, clsMusic *m, clsEvent *e, clsTimer *t)
 {
-    //ctor
+    screen = s;
+    music = m;
+    event = e;
+    timer = t;
+
+    error.set(music->loadMusic("MUSIC/MENU.mp3"));
+    if(error.get()) return error.get();
+
+    error.set(texto.init());
+    if(error.get()) return error.get();
+
+    error.set(texto.loadFont("FONTS/FreeMono.ttf",30));
+    if(error.get()) return error.get();
+
+    texto.setFontColor(WHITE);
+
+    setItems(14);
+    char ruta[]= "IMAGES/MAGOINTRO/00.jpg";
+    for(int d=0; d<2; d++)
+    {
+        ruta[17] = d + 48;
+        for(int u=0; u<10; u++)
+        {
+            ruta[18] = u + 48;
+            setI(d*10+u);
+
+            error.set(load(ruta));
+            if(error.get())
+                return error.get();
+
+            if(d*10+u == 13)
+                break;
+        }
+    }
+    setI(0);
+
+    return error.get();
 }
+
+void clsMenu::opciones()
+{
+    paste(screen->getPtr());
+    if(getI() == 13)
+        setI(0);
+    else
+        setI(getI() + 1);
+}
+
+int clsMenu::run()
+{
+    error.set(0);
+
+    music->playMusic(-1);
+
+    texto.write("Menu de opciones",400,200,screen->getPtr());
+
+    while(true)
+    {
+        if(event->wasEvent())
+        {
+            if(event->getEventType() == SDL_QUIT || event->getKey() == KEY_ESCAPE)
+            {
+                music->closeMusic();
+                return -1;
+            }
+            if(event->getKey() == KEY_ENTER)
+            {
+                music->closeMusic();
+                return 0;
+            }
+        }
+        timer->wait(150);
+        opciones();
+        screen->refresh();
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
