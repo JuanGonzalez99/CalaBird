3aa93786cec0a47916ff74e33acced71a1fb05e1
Javier Pereyra <javier.pereyra183@gmail.com>
2018-11-15 22:44:46 +0000

    headers

--- src/clsJuego.cpp
+++ src/clsJuego.cpp
@@ -1,4 +1,22 @@
-
+//#############################################################################
+// ARCHIVO             : clsMotor.cpp
+// AUTOR               : Juan Agustin Gonzalez
+// VERSION             : 1.0.
+// FECHA DE CREACION   : 06/10/2018.
+// ULTIMA ACTUALIZACION: 15/11/2018.          .
+// LICENCIA             : GPL (General Public License) - Version 3.
+//=============================================================================
+// SISTEMA OPERATIVO   : Linux / Windows.
+// IDE                 : Code::Blocks - 17.12.
+// COMPILADOR          : GNU GCC Compiler (Linux) / MinGW (Windows).
+//=============================================================================
+// DESCRIPCION: Este archivo contiene la definición de los métodos de la clas
+//              "clsMotor".
+//
+//              "clsMotor" consta de los objetos necesarios para levantar el
+//              modo gráfico, la presentación, el juego y la salida.
+//
+///////////////////////////////////////////////////////////////////////////////
 #include "clsJuego.h"
 
 int clsJuego::init(clsScreen* scr, clsEvent* ev, clsMusic* mus)
